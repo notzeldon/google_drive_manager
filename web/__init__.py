3139b29130c8dfc87d3ebb03680513fec8044fc2
ZaArs <zaars.x@gmail.com>
2020-04-22 17:21:18 +0300

    Try deploy to heroku

--- web/__init__.py
+++ web/__init__.py
@@ -1,4 +1,4 @@
-import argparse
+# import argparse
 
 from aiohttp import web
 import asyncio
@@ -12,17 +12,17 @@ try:
 except ImportError:
     print('Library uvloop is not available')
 
-parser = argparse.ArgumentParser(description='Manager project')
-parser.add_argument('--host', help='Host to listen', default='0.0.0.0')
-parser.add_argument('--port', help='Port to accept connections', default=8080)
+# parser = argparse.ArgumentParser(description='Manager project')
+# parser.add_argument('--host', help='Host to listen', default='0.0.0.0')
+# parser.add_argument('--port', help='Port to accept connections', default=8080)
 # parser.add_argument('--reload', action='store_true', help='Autoreload code on change')
 # parser.add_argument('-c', '--config', type=argparse.FileType('r'), help='Path to config file')
 
-parser.add_argument('--noauth_local_webserver', action='store_true', help='')
+# parser.add_argument('--noauth_local_webserver', action='store_true', help='')
 
-args = parser.parse_args()
+# args = parser.parse_args()
 
-app = create_app(config=load_config(args.config))
+app = create_app()
 
-web.run_app(app, host=args.host, port=args.port)
+web.run_app(app, host='0.0.0.0', port=8080)
 
