45388ca7c75e78475a244c3ab288a72e37b99e15
ZaArs <zaars.x@gmail.com>
2020-04-22 17:12:55 +0300

    Try deploy to heroku

--- web/__init__.py
+++ web/__init__.py
@@ -15,19 +15,14 @@ except ImportError:
 parser = argparse.ArgumentParser(description='Manager project')
 parser.add_argument('--host', help='Host to listen', default='0.0.0.0')
 parser.add_argument('--port', help='Port to accept connections', default=8080)
-parser.add_argument('--reload', action='store_true', help='Autoreload code on change')
-parser.add_argument('-c', '--config', type=argparse.FileType('r'), help='Path to config file')
+# parser.add_argument('--reload', action='store_true', help='Autoreload code on change')
+# parser.add_argument('-c', '--config', type=argparse.FileType('r'), help='Path to config file')
 
 parser.add_argument('--noauth_local_webserver', action='store_true', help='')
 
 args = parser.parse_args()
 
 app = create_app(config=load_config(args.config))
-
-if args.reload:
-    print('Start with code reload')
-    import aioreloader
-    aioreloader.start()
 
 web.run_app(app, host=args.host, port=args.port)
 
