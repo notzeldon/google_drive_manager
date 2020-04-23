11961a4a7133f71746fcc7ed701882bff0acbe5f
ZaArs <zaars.x@gmail.com>
2020-04-23 17:57:55 +0300

    Try deploy to heroku =_=

--- manager/middlewares.py
+++ manager/middlewares.py
@@ -49,35 +49,33 @@ async def google_drive_middleware(app, handler):
 
 
         if not check_path(request.path):
+            pass
+        import google.oauth2.credentials
+        import google_auth_oauthlib.flow
 
-            import google.oauth2.credentials
-            import google_auth_oauthlib.flow
+        # Use the client_secret.json file to identify the application requesting
+        # authorization. The client ID (from that file) and access scopes are required.
 
-            # Use the client_secret.json file to identify the application requesting
-            # authorization. The client ID (from that file) and access scopes are required.
+        flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(get_client_id_file(), SCOPES)
 
-            flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-                get_client_id_file(),
-                SCOPES)
+        # Indicate where the API server will redirect the user after the user completes
+        # the authorization flow. The redirect URI is required. The value must exactly
+        # match one of the authorized redirect URIs for the OAuth 2.0 client, which you
+        # configured in the API Console. If this value doesn't match an authorized URI,
+        # you will get a 'redirect_uri_mismatch' error.
+        # TODO: Change
+        flow.redirect_uri = 'https://gdt-manager.herokuapp.com/oauth2callback'
 
-            # Indicate where the API server will redirect the user after the user completes
-            # the authorization flow. The redirect URI is required. The value must exactly
-            # match one of the authorized redirect URIs for the OAuth 2.0 client, which you
-            # configured in the API Console. If this value doesn't match an authorized URI,
-            # you will get a 'redirect_uri_mismatch' error.
-            # TODO: Change
-            flow.redirect_uri = 'https://gdt-manager.herokuapp.com/oauth2callback'
+        # Generate URL for request to Google's OAuth 2.0 server.
+        # Use kwargs to set optional request parameters.
+        authorization_url, state = flow.authorization_url(
+            # Enable offline access so that you can refresh an access token without
+            # re-prompting the user for permission. Recommended for web server apps.
+            access_type='offline',
+            # Enable incremental authorization. Recommended as a best practice.
+            include_granted_scopes='true')
 
-            # Generate URL for request to Google's OAuth 2.0 server.
-            # Use kwargs to set optional request parameters.
-            authorization_url, state = flow.authorization_url(
-                # Enable offline access so that you can refresh an access token without
-                # re-prompting the user for permission. Recommended for web server apps.
-                access_type='offline',
-                # Enable incremental authorization. Recommended as a best practice.
-                include_granted_scopes='true')
-
-            return web.HTTPFound(authorization_url)
+        return web.HTTPFound(authorization_url)
 
         return await handler(request)
 
