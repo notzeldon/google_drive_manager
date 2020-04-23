a34a1aace6d39759594cbff8f0bab51153067d42
ZaArs <zaars.x@gmail.com>
2020-04-23 17:48:21 +0300

    Try deploy to heroku =_=

--- manager/app.py
+++ manager/app.py
@@ -22,7 +22,7 @@ async def create_app(config=None):
 
     app = web.Application(middlewares=[
         session_middleware(EncryptedCookieStorage(secret_key)),
-        google_drive_middleware,
+        # google_drive_middleware,
         authorize_middleware,
     ])
 
