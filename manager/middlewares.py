907d7f9400a3a047234f4e6bfd07c4d9ca15b206
ZaArs <zaars.x@gmail.com>
2020-04-23 19:08:59 +0300

    Try deploy to heroku =_=

--- manager/middlewares.py
+++ manager/middlewares.py
@@ -9,7 +9,7 @@ async def authorize_middleware(app, handler):
     async def middleware(request):
         def check_path(path):
             result = True
-            for r in ['/login', '/register', '/logout', '/api']:
+            for r in ['/login', '/register', '/logout', '/api', '/auth', '/oauth']:
                 if path.startswith(r):
                     result = False
             if path == '/':
@@ -37,26 +37,16 @@ async def authorize_middleware(app, handler):
 async def google_drive_middleware(app, handler):
 
     async def middleware(request):
+        session = await get_session(request)
 
-        def check_path(path):
-            result = True
-            for r in ['/oauth2callback']:
-                if path.startswith(r):
-                    result = False
-            return result
+        if 'credentials' not in session(request):
+            # flow.redirect_uri = 'http://gdt-manager.herokuapp.com/oauth2callback'
+            #
+            # authorization_url, state = flow.authorization_url(
+            #     access_type='offline',
+            #     include_granted_scopes='true')
 
-        if not check_path(request.path):
-            import google_auth_oauthlib.flow
-
-            flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(get_client_id_file(), SCOPES)
-
-            flow.redirect_uri = 'http://gdt-manager.herokuapp.com/oauth2callback'
-
-            authorization_url, state = flow.authorization_url(
-                access_type='offline',
-                include_granted_scopes='true')
-
-            raise web.HTTPFound(authorization_url)
+            raise web.HTTPFound(request.app.router['authorize'].url_for())
 
         else:
             return await handler(request)
